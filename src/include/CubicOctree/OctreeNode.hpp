740e1f3bfdff15f69cbd65ea0f55ce5723a02771
Mark Nefedov <mark_nefedov@outlook.com>
2020-06-04 20:54:21 +0300

    No reason to use pointer for types smaller than pointer.

--- src/include/CubicOctree/OctreeNode.hpp
+++ src/include/CubicOctree/OctreeNode.hpp
@@ -9,16 +9,62 @@
 // X right, Y up, Z forward
 
 namespace Teroleon {
+
+    template<typename T>
+    using sharedPtrTypeSelector = typename std::conditional<sizeof(T) <= sizeof(std::shared_ptr<T>), T, std::shared_ptr<T>>::type;
+
+    template<typename T>
+    class swStorage{
+        sharedPtrTypeSelector<T> data;
+    public:
+        bool operator==(const swStorage &rhs) const {
+            if constexpr (std::is_pointer_v<sharedPtrTypeSelector<T>>)
+                return *data == *rhs.data;
+            else
+                return data == rhs.data;
+        }
+        bool operator!=(const swStorage &rhs) const {
+            return !(rhs == *this);
+        }
+
+        operator T() const {
+            if constexpr (std::is_pointer_v<sharedPtrTypeSelector<T>>)
+                return *data;
+            else
+                return data;
+        }
+
+        swStorage(){
+            if constexpr (std::is_pointer_v<sharedPtrTypeSelector<T>>)
+                data = std::make_shared<T>();
+        }
+
+        explicit swStorage(const T& newData){
+            if constexpr (std::is_pointer_v<sharedPtrTypeSelector<T>>)
+                data = std::make_shared<T>(newData);
+        }
+
+        void operator=(const T& other_data){
+            if constexpr (std::is_pointer_v<sharedPtrTypeSelector<T>>)
+                *data = other_data;
+            else
+                data = other_data;
+        }
+    };
+
     template<typename T> requires std::equality_comparable<T>
     struct OctreeNode {
-        std::variant<std::shared_ptr<OctreeNode<T>[]>, std::shared_ptr<T>> data;
+        std::variant<std::shared_ptr<OctreeNode<T>[]>, swStorage<T>> data;
         bool isLeaf;
+        OctreeNode() : isLeaf(true){};
+        OctreeNode(const T& newData, bool leaf):data(swStorage<T>(newData)), isLeaf(leaf) {
+        }
 
         /// Subdivide current node and copy it content to new leaves.
         void Subdivide() noexcept {
             if (!isLeaf) return;
             isLeaf = false;
-            auto savedValue = std::get<std::shared_ptr<T>>(data);
+            auto savedValue = std::get<swStorage<T>>(data);
             data = std::shared_ptr<OctreeNode<T>[]>(new OctreeNode<T>[8]);
             auto subNodes = std::get<std::shared_ptr<OctreeNode<T>[]>>(data);
             for (uint8_t i = 0; i < 8; i++) {
@@ -41,9 +87,9 @@ namespace Teroleon {
             for (uint8_t i = 0; i < 8; i++) {
                 if (!subNodes[i].isLeaf) return false; // all subnodes must be leaves
             }
-            std::shared_ptr<T> controlValue = std::get<std::shared_ptr<T>>(subNodes[0].data);
+            swStorage<T> controlValue = std::get<swStorage<T>>(subNodes[0].data);
             for (uint8_t i = 0; i < 8; i++) {
-                if (*std::get<std::shared_ptr<T>>(subNodes[i].data) != *controlValue) return false;
+                if (std::get<swStorage<T>>(subNodes[i].data) != controlValue) return false;
             }
             isLeaf = true;
             data = controlValue;
